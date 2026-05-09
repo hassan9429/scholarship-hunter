6fbd812414795b7d15dc53120b0c188b283e964a
Hassan Iqbal Sheikh <157453556+hassan9429@users.noreply.github.com>
2026-05-09 17:47:02 +0500

    Update app.py

--- app.py
+++ app.py
@@ -30,6 +30,16 @@ components.html(
     height=0,
 )
 
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-KZ6LXW693R"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-KZ6LXW693R');
+</script>
+
 st.markdown("""
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Sora:wght@300;400;500;600;700;800&family=JetBrains+Mono:wght@400;500&display=swap');
